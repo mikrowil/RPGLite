0ff481ca54bebd4133aa2db8acd764757f606516
Michael <Michaelr9wilson@gmail.com>
2020-12-29 17:51:40 -0800

    Created Store view and Store entity class

---
+++ inc/Entities/Store.tsx
@@ -0,0 +1,58 @@
+import Item from "./Item";
+
+class Store {
+    private items:Item[]
+
+
+    constructor(items: Item[]) {
+        this.items = items;
+    }
+
+    public getItems(){
+        return this.items
+    }
+
+    public addItem(newEntry:Item){
+        this.items.push(newEntry)
+    }
+
+    public setItems(items:Item[]){
+        this.items = items
+    }
+
+    public isEmpty(){
+        return this.items.length <= 0
+    }
+
+    public sortBy(property:String){
+
+
+        this.items = this.items.sort((i1,i2)=>{
+            if(property === "name"){
+                if(i1.name > i2.name){
+                    return -1
+                }else if(i1.name <i2.name){
+                    return 1
+                }else {
+                    return 0
+                }
+            }else if(property === "value"){
+                if(i1.value > i2.value){
+                    return -1
+                }else if(i1.value <i2.value){
+                    return 1
+                }else {
+                    return 0
+                }
+            }else {
+                if(i1.id > i2.id){
+                    return -1
+                }else if(i1.id <i2.id){
+                    return 1
+                }else {
+                    return 0
+                }
+            }
+        })
+    }
+}
---
+++ inc/Views/StoreScreen.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Text, View, StyleSheet,TouchableOpacity} from 'react-native'
+import {widthPercentageToDP as wd, heightPercentageToDP as hd} from 'react-native-responsive-screen'
+
+
+interface IProps {
+
+}
+
+interface IState {
+
+}
+
+
+export default class VictoryScreen extends React.Component<IProps,IState>{
+
+
+    constructor(props:IProps) {
+        super(props);
+
+        this.state = {
+
+        }
+
+    }
+
+
+    render() {
+
+        return(
+            <View>
+                
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+
+
+})
